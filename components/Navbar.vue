b88275db77b027c0baaadd08531dbdf9092d8470
ikeohachidi <ikeohachidi@gmail.com>
2022-01-08 12:05:16 +0100

    feat: update navigation

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,8 +3,28 @@
         <li><nuxt-link to="/">ikeoha.com</nuxt-link></li>
         <li><nuxt-link to="/post">posts</nuxt-link></li>
         <li><a href="https://github.com/ikeohachidi" target="_blank">projects</a></li>
+
+        <li class="ml-auto" @click="goToExternalPage('github')">
+            <i class="ri-github-fill ri-lg"></i>
+            <span class="ml-2">Github</span>
+        </li>
+        <li @click="goToExternalPage('twitter')">
+            <i class="ri-twitter-fill ri-lg"></i>
+            <span class="ml-2">Twitter</span>
+        </li>
     </ul>
 </template>
+
+<script>
+export default {
+    methods: {
+        goToExternalPage(page) {
+            if (page === 'github') window.open('https://github.com/ikeohachidi', '_blank');
+            if (page === 'twitter') window.open('https://twitter.com/ikeoha_chidi', '_blank');
+        }
+    }
+}
+</script>
 
 <style lang="postcss" scoped>
 ul {
@@ -16,6 +36,6 @@ ul li:first-of-type {
 } 
 
 ul li {
-    @apply px-4;
+    @apply px-4 flex items-center cursor-pointer;;
 }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@/assets/css/main.css'
+    '@/assets/css/main.css',
+    'remixicon/fonts/remixicon.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
